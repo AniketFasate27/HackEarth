c2626c93a55e067bc21aba4d717918888aa6451f
Aniket Fasate <49206752+AniketFasate27@users.noreply.github.com>
2021-09-04 11:27:51 +0530

    Add files via upload

---
+++ vdsvdsv.py
@@ -0,0 +1,14 @@
+def MainProgram(f):
+    def program_1():
+        print("1",end=' ')
+
+        f()
+        print("2")
+
+    return program_1
+
+def program_2():
+    print("Python",end=' ')
+
+newf =MainProgram(program_2)
+newf()
